ea0d0a981b4ae44721c8355b90b2e07f29b4ac49
Zoe-997 <thuminh10011997@mail.com>
2024-08-08 15:38:18 +0700

    fix: banner top

--- app/components/Sections/BannerTop.tsx
+++ app/components/Sections/BannerTop.tsx
@@ -47,7 +47,7 @@ const BannerTop = () => {
       >
         <div
           className={`z-[1] relative ${
-            active ? "w-[50px] h-[50px]" : "w-[30px] h-[30;px]"
+            active ? "w-[50px] h-[50px]" : "w-[30px] h-[30px]"
           } overflow-hidden transition-all duration-500 cursor-pointer mx-auto`}
           onClick={() => onClick()}
         >
