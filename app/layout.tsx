3dfa8c84d124a06bd64bb731ab241b3eb127e0a7
Zoe-997 <thuminh10011997@mail.com>
2024-08-09 09:34:34 +0700

    fix: favicon get to backend

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import { fontBase } from "@/app/libs/fonts";
 import "react-multi-carousel/lib/styles.css";
 
@@ -6,6 +7,14 @@ import Header from "@/app/components/Header/Header";
 import Footer from "@/app/components/Footer/Footer";
 import BackToTop from "@/app/components/Commons/BackToTop";
 import Messages from "@/app/components/Commons/Messages";
+
+import LogoImage from "@/public/images/logo.png";
+
+export const metadata: Metadata = {
+  icons: {
+    icon: `${LogoImage}`,
+  },
+};
 
 export default function RootLayout({
   children,
